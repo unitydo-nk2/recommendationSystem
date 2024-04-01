037b767e54ddec8424e55bc30977e323a5210569
phxsrp <phxsrp@gmail.com>
2024-04-01 09:37:59 +0700

    [UPDATE] update path

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,9 @@ WORKDIR /app
 # Copy the current directory contents into the container at /app
 COPY . /app
 
+
+
+
 # Install any needed dependencies specified in requirements.txt
 RUN pip install --no-cache-dir -r requirements.txt
 
--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from recommendationSystem import recommender_engine
 
 app = Flask(__name__)
 
-@app.route('/getRecommends/<int:user_id>', methods=['GET'])
+@app.route('api/getRecommends/<int:user_id>', methods=['GET'])
 def getRecommendActivity(user_id):
+    print("Recommendation for user called")
     return recommender_engine(user_id)
 
 
