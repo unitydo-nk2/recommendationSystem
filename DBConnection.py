cb48c6837aac3d984c293a8498a6a5f8390ed49f
phxsrp <phxsrp@gmail.com>
2024-04-28 04:53:28 +0700

    [UPDATE] database connection

--- DBConnection.py
+++ DBConnection.py
@@ -1,24 +1,34 @@
 import mysql.connector
 import pandas as pd
 
-mydb = mysql.connector.connect(
-  # host="10.4.85.19",
-  host="172.26.0.3",
-  user="root",
-  password="dreamtongphop",
-  database="unityDoDB"
-)
+# mydb = mysql.connector.connect(
+#   host="10.4.85.19",
+#   # host="172.26.0.3",
+#   user="root",
+#   password="dreamtongphop",
+#   database="unityDoDB"
+# )
+
+config = {
+  # 'host': "10.4.85.19",
+  'host': "172.26.0.3",
+  'user': "root",
+  'password': "dreamtongphop",
+  'database': "unityDoDB"
+}
 
 def get_activitywithcategoryview():
-  mycursor = mydb.cursor()
-  mycursor.execute("SELECT * FROM activityWithCategoryView")
-  myresult = mycursor.fetchall()
-  df = pd.DataFrame(myresult, columns=mycursor.column_names)
+  with mysql.connector.connect(**config) as cnx:
+    mycursor = cnx.cursor()
+    mycursor.execute("SELECT * FROM activityWithCategoryView")
+    myresult = mycursor.fetchall()
+    df = pd.DataFrame(myresult, columns=mycursor.column_names)
   return df
 
 def get_usercategoryrankingview():
-  mycursor = mydb.cursor()
-  mycursor.execute("SELECT * FROM userCategoryRankingView")
-  myresult = mycursor.fetchall()
-  df = pd.DataFrame(myresult, columns=mycursor.column_names)
+  with mysql.connector.connect(**config) as cnx:
+    mycursor = cnx.cursor()
+    mycursor.execute("SELECT * FROM userCategoryRankingView")
+    myresult = mycursor.fetchall()
+    df = pd.DataFrame(myresult, columns=mycursor.column_names)
   return df
